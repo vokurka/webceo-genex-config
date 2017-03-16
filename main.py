a3225ae20abea9313d265108ee706994db372fb0
Vojtech Kurka <vokurka@keboola.com>
2017-03-16 14:51:01 +0800

    base version

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-import csv, pip, pprint
+import csv, pip, pprint, json
 from keboola import docker
+pip.main(['install', 'requests'])
+import requests
 
 # Getting configuration
 cfg = docker.Config('/data/')
 parameters = cfg.get_parameters()
 config = {}
-configFields = ['#sapi_token', '#webceo_token']
+configFields = ['#sapi_token', '#webceo_token', 'component_id', 'config_id']
 
 for field in configFields:
 	config[field] = parameters.get(field)
@@ -13,4 +15,44 @@ for field in configFields:
 	if not config[field]:
 		raise Exception('Missing mandatory configuration field: '+field)
 
+# Loading list of projects I want to generate the configuration for
 projects = list(csv.DictReader(open('/data/in/tables/projects.csv', 'r', encoding='utf-8')))
+
+# Base part of configuration - envelope
+genex_config = {
+  "parameters": {
+    "api": {
+      "baseUrl": "https://online.webceo.com/",
+      "name": "webceo"
+    },
+    "config": {
+      "id": "webceo",
+      "debug": 1,
+      "jobs": "placeholder"
+    }}}
+
+# List of all generated jobs
+jobs = []
+
+# Go through all projects and for each one create a job
+for p in projects:
+	jobs.append({
+      "endpoint": "api/",
+      "dataType": "rankings",
+      "dataField": ".",
+      "method": "POST",
+      "params": {
+        "method": "get_rankings",
+        "key": config['#webceo_token'],
+        "data": {
+          "project": p['project']
+     }}})
+
+# Put the jobs array to the correct place in GenEx configuration envelope
+genex_config['parameters']['config']['jobs'] = jobs;
+
+# Update the configuration in KBC
+r = requests.put("https://connection.keboola.com/v2/storage/components/"+config['component_id']+"/configs/"+config['config_id'],
+	headers={"X-StorageApi-Token": config['#sapi_token']}, 
+	files=dict(name='WebCEO Rankings', configuration=json.dumps(genex_config)
+	))
